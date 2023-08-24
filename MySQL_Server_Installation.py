5dd6329f8de106951bd06666d44290a827625ea9
OmkarGaikwad21 <109943757+OmkarGaikwad21@users.noreply.github.com>
2023-08-24 19:15:16 +0530

    Update MySQL_Server_Installation.py

--- MySQL_Server_Installation.py
+++ MySQL_Server_Installation.py
@@ -1,22 +1,31 @@
 import subprocess
 
-# Install MySQL repository
-subprocess.run("wget https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
-subprocess.run("sudo rpm -ivh mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
+# Step 1: Download MySQL repository
+subprocess.run(["wget", "https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm"], check=True)
 
-# Update and install MySQL Server
-subprocess.run("sudo yum update -y", shell=True, check=True)
-subprocess.run("sudo yum install mysql-server --nogpgcheck -y", shell=True, check=True)
-subprocess.run("sudo systemctl start mysqld", shell=True, check=True)
+# Step 2: Install MySQL repository
+subprocess.run(["sudo", "rpm", "-ivh", "mysql80-community-release-el7-9.noarch.rpm"], check=True)
 
-# Get temporary password
-result = subprocess.run("sudo grep 'temporary password' /var/log/mysqld.log", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+# Step 3: Update packages
+subprocess.run(["sudo", "yum", "update", "-y"], check=True)
+
+# Step 4: Install MySQL Server
+subprocess.run(["sudo", "yum", "install", "mysql-server", "--nogpgcheck", "-y"], check=True)
+
+# Step 5: Start MySQL service
+subprocess.run(["sudo", "systemctl", "start", "mysqld"], check=True)
+
+# Step 6: Check MySQL service status
+subprocess.run(["sudo", "systemctl", "status", "mysqld"], check=True)
+
+# Step 7: Get temporary password
+result = subprocess.run(["sudo", "grep", "'temporary password'", "/var/log/mysqld.log"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 temp_password = result.stdout.split()[-1]
 
-# Secure MySQL installation
+# Step 8: Secure MySQL installation
 secure_install_cmd = (
-    f"echo -e 'Omkar@123\\nOmkar@123\\n{temp_password}\\nY\\nY\\nY\\nn\\nY' | "
-    f"sudo mysql_secure_installation"
+    "echo -e 'Omkar@123\\nOmkar@123\\n{}\\nn\\nY\\nY\\nn\\nY' | "
+    "sudo mysql_secure_installation".format(temp_password)
 )
 subprocess.run(secure_install_cmd, shell=True, check=True)
 
