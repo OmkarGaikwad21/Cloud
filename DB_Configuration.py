3b32edb66549a9bf5f9eaf24799b4c2d27394acf
OmkarGaikwad21 <109943757+OmkarGaikwad21@users.noreply.github.com>
2023-08-24 17:19:20 +0530

    Update DB_Configuration.py

--- DB_Configuration.py
+++ DB_Configuration.py
@@ -1,46 +1,51 @@
 import subprocess
+import sys
 
-# Install MySQL
-subprocess.run("wget https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
-subprocess.run("sudo rpm -ivh mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
-subprocess.run("sudo yum update -y", shell=True, check=True)
-subprocess.run("sudo yum install mysql-server -y", shell=True, check=True)
-subprocess.run("sudo systemctl start mysqld", shell=True, check=True)
+def main():
 
-# Get temporary password
-result = subprocess.run("sudo grep 'temporary password' /var/log/mysqld.log", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-temp_password = result.stdout.split()[3]
+  # Install MySQL
+  subprocess.run("wget https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
+  subprocess.run("sudo rpm -ivh mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True) 
+  subprocess.run("sudo yum update -y", shell=True, check=True)
+  subprocess.run("sudo yum install mysql-server -y", shell=True, check=True)
+  subprocess.run("sudo systemctl start mysqld", shell=True, check=True)
 
-# Secure MySQL installation
-sql_commands = [
-    "y",  # Remove anonymous users
-    "Y",  # Disallow root login remotely
-    "n",  # Remove test database and access it
-    "Y",  # Reload privilege tables
-]
-sql_commands_str = "\\n".join(sql_commands)
+  # Get temp password
+  result = subprocess.run("sudo grep 'temporary password' /var/log/mysqld.log", shell=True, capture_output=True, text=True)
+  temp_password = result.stdout.split()[3]
+  print(f"Temporary password: {temp_password}")
 
-secure_install_cmd = (
-    f"echo -e 'Omkar@123\\nOmkar@123\\n{temp_password}\\n{sql_commands_str}' | sudo mysql_secure_installation"
-)
-subprocess.run(secure_install_cmd, shell=True, check=True, env={"temp_password": temp_password})
+  # Secure MySQL installation
+  sql_commands = [
+    "SET PASSWORD = 'Omkar@123';",
+    "ALTER USER 'root'@'localhost' PASSWORD EXPIRE NEVER;", 
+    "drop database if exists test;",
+    "flush privileges;"
+  ]
+  subprocess.run("echo '\n".join(sql_commands) + f"' | mysql -u root -p{temp_password}", shell=True, check=True)
 
-# Create databases and users
-databases = ["scm", "hive", "hue", "rman", "navs", "navms", "oozie", "actmo", "sentry", "ranger"]
+  # Create databases and users
+  databases = ["scm", "hive", "hue", "rman", "navs", "navms", "oozie", "actmo", "sentry", "ranger"]
 
-for db in databases:
+  for db in databases:
     sql = f"CREATE DATABASE {db} DEFAULT CHARACTER SET utf8;"
-    subprocess.run(f"echo '{sql}' | mysql -u root -pOmkar@123", shell=True, check=True)
+    subprocess.run(f"echo '{sql}' | mysql -u root -pOmkar@123", shell=True, check=True)  
+
     sql = f"CREATE USER '{db}'@'%' IDENTIFIED BY 'Omkar@123';"
     subprocess.run(f"echo '{sql}' | mysql -u root -pOmkar@123", shell=True, check=True)
-    sql = f"GRANT ALL PRIVILEGES ON {db}.* TO '{db}'@'%';"
+
+    sql = f"GRANT ALL PRIVILEGES ON {db}.* TO '{db}'@'%';" 
     subprocess.run(f"echo '{sql}' | mysql -u root -pOmkar@123", shell=True, check=True)
 
-# Create temp user
-temp_user_sql = """
+  # Create temp user
+  temp_user_sql = """
     CREATE USER 'temp'@'%' IDENTIFIED BY 'Omkar@123';
     GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO 'temp'@'%' WITH GRANT OPTION;
-"""
-subprocess.run(f"echo '{temp_user_sql}' | mysql -u root -pOmkar@123", shell=True, check=True)
+  """
 
-print("MySQL setup completed!")
+  subprocess.run(f"echo '{temp_user_sql}' | mysql -u root -pOmkar@123", shell=True, check=True)
+
+  print("MySQL setup completed!")
+
+if __name__ == "__main__":
+  main()
