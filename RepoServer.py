2edcbb2acacfbd50988981b3bc56a594b73d2fc5
OmkarGaikwad21 <109943757+OmkarGaikwad21@users.noreply.github.com>
2023-08-25 01:16:55 +0530

    Update RepoServer.py

--- RepoServer.py
+++ RepoServer.py
@@ -15,11 +15,12 @@ job_list = [
 'wget https://archive.cloudera.com/cdh7/7.1.7/parcels/KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel.sha256',
 'wget https://archive.cloudera.com/cdh7/7.1.7/parcels/manifest.json',
 
+'sudo mkdir -p /var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels/',
 'sudo mkdir -p /var/www/html/cloudera-repos/cm7/',
 
-'sudo mv CDH-7.1.7-1.cdh7.1.7.p0.15945976-el7.parcel* /var/www/html/cloudera-repos/cm7/',
-'sudo mv KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel* /var/www/html/cloudera-repos/cm7/',
-'sudo mv manifest.json /var/www/html/cloudera-repos/cm7/',
+'sudo mv CDH-7.1.7-1.cdh7.1.7.p0.15945976-el7.parcel* /var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels ',
+'sudo mv KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel* /var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels ',
+'sudo mv manifest.json /var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels ',
 
 'sudo tar -xzvf cm7.4.4-redhat7.tar.gz -C /var/www/html/cloudera-repos/cm7/',
 
