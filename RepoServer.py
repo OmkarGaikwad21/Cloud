17ad7f84c3cd02b33d31f3300304aa255082bdcf
OmkarGaikwad21 <109943757+OmkarGaikwad21@users.noreply.github.com>
2023-08-25 01:59:55 +0530

    Update RepoServer.py

--- RepoServer.py
+++ RepoServer.py
@@ -11,20 +11,24 @@ subprocess.run(["sudo", "mkdir", "-p", "/var/www/html/cloudera-repos/cm7/"])
 subprocess.run(["sudo", "wget", "https://archive.cloudera.com/cm7/7.4.4/repo-as-tarball/cm7.4.4-redhat7.tar.gz"])
 subprocess.run(["sudo", "tar", "-xzvf", "cm7.4.4-redhat7.tar.gz", "-C", "/var/www/html/cloudera-repos/cm7/"])
 
-# Download CDH parcels and files
+# Create the directory for CDH parcels
 subprocess.run(["sudo", "mkdir", "-p", "/var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels"])
+
+# Download CDH parcels and files
 parcel_urls = [
     "https://archive.cloudera.com/cdh7/7.1.7/parcels/CDH-7.1.7-1.cdh7.1.7.p0.15945976-el7.parcel",
     "https://archive.cloudera.com/cdh7/7.1.7/parcels/CDH-7.1.7-1.cdh7.1.7.p0.15945976-el7.parcel.sha1",
     "https://archive.cloudera.com/cdh7/7.1.7/parcels/CDH-7.1.7-1.cdh7.1.7.p0.15945976-el7.parcel.sha256",
     "https://archive.cloudera.com/cdh7/7.1.7/parcels/KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel",
     "https://archive.cloudera.com/cdh7/7.1.7/parcels/KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel.sha",
-    "https://archive.cloudera.com/cdh7/7.1.7/parcels/KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel.sha1",
-    "https://archive.cloudera.com/cdh7/7.1.7/parcels/manifest.json"
+    "https://archive.cloudera.com/cdh7/7.1.7/parcels/KEYTRUSTEE_SERVER-7.1.7.0-1.keytrustee7.1.7.0.p0.15945976-el7.parcel.sha1"
 ]
 
 for url in parcel_urls:
     subprocess.run(["sudo", "wget", url, "-P", "/var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels"])
+
+# Download manifest.json
+subprocess.run(["sudo", "wget", "https://archive.cloudera.com/cdh7/7.1.7/parcels/manifest.json", "-P", "/var/www/html/cloudera-repos/cdh7/7.1.7.0/parcels"])
 
 # Start Apache service
 subprocess.run(["sudo", "systemctl", "start", "apache2"])
