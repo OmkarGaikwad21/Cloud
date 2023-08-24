c6bb053f1f52ff71a2b4105ed1018130862db3a7
OmkarGaikwad21 <109943757+OmkarGaikwad21@users.noreply.github.com>
2023-08-24 14:36:57 +0530

    Add files via upload

---
+++ CM_Configuration.py
@@ -0,0 +1,32 @@
+import subprocess
+import sys
+
+# Configure Cloudera Manager repo
+with open('/etc/yum.repos.d/cloudera-manager.repo', 'w') as f:
+  f.write("""
+[cloudera-manager]
+name=Cloudera Manager
+baseurl=http://your_repo_privateIP/cm/7.x.x/ 
+gpgkey =http://your_repo_privateIP/cm/7.x.x/RPM-GPG-KEY-cloudera
+gpgcheck = 0
+""")
+
+# Install Cloudera Manager
+subprocess.run("sudo yum clean all", shell=True, check=True)
+subprocess.run("sudo yum makecache", shell=True, check=True)
+subprocess.run("sudo yum install cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server -y", shell=True, check=True)
+
+# Prepare database 
+db_host = "your_db_host_privateIP_of_DNS" 
+db_name = "scm"
+db_user = "scm"
+db_password = "Omkar@123"
+
+subprocess.run(f"sudo /opt/cloudera/cm/schema/scm_prepare_database.sh mysql -h {db_host} {db_name} {db_user} {db_password}", shell=True, check=True)
+
+# Start Cloudera Manager
+subprocess.run("sudo systemctl start cloudera-scm-server", shell=True, check=True)
+subprocess.run("sudo systemctl enable cloudera-scm-server", shell=True, check=True)
+subprocess.run("sudo tail -f /var/log/cloudera-scm-server/cloudera-scm-server.log", shell=True)
+
+print("Cloudera Manager installed and configured!")
---
+++ DB_Configuration.py
@@ -0,0 +1,33 @@
+import subprocess
+
+# Install MySQL
+subprocess.run("wget https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
+subprocess.run("sudo rpm -ivh mysql80-community-release-el7-9.noarch.rpm", shell=True, check=True)
+subprocess.run("sudo yum update -y", shell=True, check=True)
+subprocess.run("sudo yum install mysql-server -y", shell=True, check=True)
+subprocess.run("sudo systemctl start mysqld", shell=True, check=True)
+
+# Get temporary password
+result = subprocess.run("sudo grep 'temporary password' /var/log/mysqld.log", shell=True, capture_output=True, check=True)
+temp_password = result.stdout.decode('utf-8').split()[3]
+
+# Secure MySQL installation
+sql_commands = [f"{temp_password}", "Omkar@123", "Omkar@123", "n", "Y", "Y", "n", "Y"]
+subprocess.run("echo '{}' | sudo mysql_secure_installation".format('\\n'.join(sql_commands)), shell=True, check=True)
+
+# Create databases and users
+databases = ["scm", "hive", "hue", "rman", "navs", "navms", "oozie", "actmo", 
+             "sentry", "ranger"]
+
+for db in databases:
+    sql = f"""
+        CREATE DATABASE {db} DEFAULT CHARACTER SET utf8;
+        CREATE USER '{db}'@'%' IDENTIFIED BY 'Omkar@123';
+        GRANT ALL PRIVILEGES ON {db}.* TO '{db}'@'%';
+    """
+    subprocess.run(f"echo '{sql}' | mysql -u root -pOmkar@123", shell=True, check=True)
+
+# Create temp user  
+subprocess.run("echo 'CREATE USER temp@'%' IDENTIFIED BY 'Omkar@123'; GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO 'temp'@'%' WITH GRANT OPTION;' | mysql -u root -pOmkar@123", shell=True, check=True)
+
+print("MySQL setup completed!")
