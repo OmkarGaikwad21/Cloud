b7a685f51e91c5cc09645962119bf6a946bf2ca9
OmkarGaikwad21 <109943757+OmkarGaikwad21@users.noreply.github.com>
2023-10-18 01:42:21 +0530

    Update MySQL_Server_Installation.py

--- MySQL_Server_Installation.py
+++ MySQL_Server_Installation.py
@@ -1,19 +1,63 @@
 import subprocess
 
 # Download MySQL repository RPM
-subprocess.run(["wget", "https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm"], check=True)
+subprocess.call(["wget", "https://dev.mysql.com/get/mysql80-community-release-el7-9.noarch.rpm"])
 
-# Install MySQL repository
-subprocess.run(["sudo", "rpm", "-ivh", "mysql80-community-release-el7-9.noarch.rpm"], check=True)
+# Install MySQL repository 
+subprocess.call(["sudo", "rpm", "-ivh", "mysql80-community-release-el7-9.noarch.rpm"])
 
 # Update repositories
-subprocess.run(["sudo", "yum", "update", "-y"], check=True)
+subprocess.call(["sudo", "yum", "update", "-y"])
 
-# Install MySQL Server
-subprocess.run(["sudo", "yum", "install", "mysql-server", "--nogpgcheck", "-y"], check=True)
+# Install MySQL server
+subprocess.call(["sudo", "yum", "install", "mysql-server", "--nogpgcheck", "-y"])
 
 # Start MySQL service
-subprocess.run(["sudo", "systemctl", "start", "mysqld"], check=True)
+subprocess.call(["sudo", "service", "mysqld", "start"]) 
 
-# Check MySQL service status
-subprocess.run(["sudo", "systemctl", "status", "mysqld"], check=True)
+# Restart to generate new temporary password
+subprocess.call(["sudo", "service", "mysqld", "restart"])
+
+# Get temporary password
+temp_password = subprocess.check_output(["sudo", "grep", "temporary password", "/var/log/mysqld.log"]).decode('utf-8').split(': ')[-1].strip()
+
+print("Temporary password: ", temp_password)  
+
+# Install expect
+subprocess.call(["sudo", "yum", "install", "-y", "expect"])
+
+# Create an expect script
+expect_script = f"""spawn sudo mysql_secure_installation
+
+expect "*Enter password for user root:*"
+send "{temp_password}\\r"
+
+expect "*New password:*"
+send "Omkar@123\\r"
+
+expect "*Re-enter new password:*"
+send "Omkar@123\\r"
+
+expect "*Change the password for root ? ((Press y|Y for Yes, any other key for No) :*"
+send "n\\r"
+
+expect "*Remove anonymous users? ((Press y|Y for Yes, any other key for No) :*"
+send "Y\\r"
+
+expect "*Disallow root login remotely? ((Press y|Y for Yes, any other key for No) :*"
+send "Y\\r"
+
+expect "*Remove test database and access to it? ((Press y|Y for Yes, any other key for No) :*"
+send "n\\r"
+
+expect "*Reload privilege tables now? ((Press y|Y for Yes, any other key for No) :*"
+send "Y\\r"
+
+expect eof"""
+
+with open("secure_mysql.exp", 'w') as f:
+    f.write(expect_script)
+
+# Run expect script
+subprocess.call(["expect", "./secure_mysql.exp"])
+
